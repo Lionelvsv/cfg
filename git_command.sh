5b7d79005fa738137c515bbeff4a5fac3236ad2a
lionelvsv <lionelvsv@gmail.com>
2020-04-16 21:54:36 +0530

    added git commit script

---
+++ git_command.sh
@@ -0,0 +1,42 @@
+alias config="/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME"
+config add dwmblocks/
+config add .config/alacritty/
+config add .config/awesome/
+config add .config/broot/
+config add .config/bspwm/
+config add .config/canto/
+config add .config/castero/
+config add .config/compton/
+config add .config/conky/
+config add .config/herbstluftwm/
+config add .config/nvim/
+config add .config/openbox/
+config add .config/polybar/
+config add .config/qtile/
+config add .config/rofi/
+config add .config/stumpwm/
+config add .config/sxhkd/
+config add .config/termite/
+config add .config/tig/
+config add .config/tint2/
+config add .config/vifm/
+config add .config/xmobar/
+config add .dmenu/
+config add .doom.d/
+config add .dwm/
+config add .emacs.d/
+config add .fvwm/
+config add .i3/
+config add .jwm/menus/
+config add .local/bin/
+config add .local/share/castero/
+config add .moc/
+config add .ncmpcpp/
+config add .newsboat/
+config add .surf/
+config add .xmonad/
+config add .Xresources
+config add .bashrc .jwmrc .jwmrc-mjwm .lynx.cfg .lynx.lss
+config add .spectrwm.conf .vimrc .zshrc
+config add LICENSE README.md baraction.sh new.sh org-mode-basics-in-doom-emacs.org
+config add git_command.sh
